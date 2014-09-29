c88888cd4a3f02171a33a36c4a8aecfe43d22976
Josh Pollock <jpollock412@gmail.com>
2014-09-28 20:07:32 -0400

    simplify version compares in activation.

--- pods-deploy.php
+++ pods-deploy.php
@@ -84,15 +84,15 @@ function pods_deploy_dependency_check() {
 	}
 
 	if ( ! is_array( $fail ) ) {
-		if ( ! version_compare( PODS_JSON_API_VERSION, PODS_DEPLOY_MIN_JSON_API_VERSION ) <= 0 ) {
+		if ( ! version_compare( PODS_JSON_API_VERSION, PODS_DEPLOY_MIN_JSON_API_VERSION, '>=' ) ) {
 			$fail[] = sprintf( 'Pods Deploy requires Pods JSON API version %1s or later. Current version is %2s.', PODS_DEPLOY_MIN_JSON_API_VERSION, PODS_JSON_API_VERSION );
 		}
 
-		if ( ! version_compare( PODS_VERSION, PODS_DEPLOY_MIN_PODS_VERSION ) <= 0 ) {
+		if ( ! version_compare( PODS_VERSION, PODS_DEPLOY_MIN_PODS_VERSION, '>=' ) ) {
 			$fail[] = sprintf( 'Pods Deploy requires Pods version %1s or later. Current version is %2s.', PODS_DEPLOY_MIN_PODS_VERSION, PODS_VERSION );
 		}
 
-		if ( version_compare( PHP_VERSION, '5.3.0' ) <= 0 ) {
+		if ( ! version_compare( PHP_VERSION, '5.3.0', '>=' ) ) {
 			$fail[] = sprintf( 'Pods Deploy requires PHP version %1s or later. Current version is %2s.', '5.3.0', PHP_VERSION );
 
 		}
